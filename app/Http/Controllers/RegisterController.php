af5e0d9e02a0c639d2a503b6c35223145855d5ed
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-31 19:25:28 +0200

    create register route and controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        try{
+            $validation = $request->validate([
+                'name' => 'required',
+                'email' => 'required|email',
+                'password' => 'required']);
+        }
+        catch (\Exception $exception){
+            return response()->json("error", Response::HTTP_BAD_REQUEST);
+        }
+        $user = User::firstOrCreate([ "name" => $request->get('name'),
+            "email" => $request->get('email'),
+            "password" => Hash::make( $request->get('password'))]);
+        return response()->json([
+            "token" => $user->createToken($user->name)->plainTextToken,
+            'status_code' => 200,
+            'message' => 'User created successfully'
+        ]);
+    }
+}
