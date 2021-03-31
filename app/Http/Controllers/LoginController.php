2e42ab8b32b1f50593810b4ecf32e74a2dab28c4
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-31 19:30:09 +0200

    add login route and controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function login(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+        return response()->json(["token" => $user->createToken($user->name)->plainTextToken,
+            "email" => $user->email]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+    }
+}
