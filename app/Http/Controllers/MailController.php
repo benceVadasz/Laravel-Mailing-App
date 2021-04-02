5949168be4640dfad4edf030df31d976fd66785c
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-04-02 09:00:39 +0200

    add compose method

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mail;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class MailController extends Controller
 {
-    //
+    public function composeAction(Request $request): ?JsonResponse
+    {
+        $user = new UserController;
+        try {
+            $receiverEmail = $request->get('email');
+            $senderId = auth()->user()->id;
+            $receiverId = $user->getUserIdByName($receiverEmail);
+            $subject = $request->get('subject');
+            $message = $request->get('message');
+            Mail::create(
+                ['id_user_from' => $senderId, 'id_user_to' => $receiverId,
+                    'subject' => $subject, 'message' => $message]
+            );
+            return response()->json(['inserted'], Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(['something went wrong'], Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
